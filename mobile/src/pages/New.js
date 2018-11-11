ad5c0bde726439334a8321a4805ff34cf3cb64ad
Clediano Estefenon <cledianoestefenon@gmail.com>
2018-11-10 23:50:01 -0200

    Projeto usando react-native.

--- mobile/src/pages/New.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-import { View, Text } from 'react-native';
-
-export default class New extends Component {
-    render() {
-        return <Text>New</Text>
-    }
-}
--- mobile/src/pages/Timeline.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-import { View, Text } from 'react-native';
-
-export default class Timeline extends Component {
-    render() {
-        return <Text>Timeline</Text>
-    }
-}
